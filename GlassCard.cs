05621a70469119170c2aba22c7f6192691bfcd34
jaydenlaunuru <9025846@student.zadkine.nl>
2026-04-13 18:12:44 +0200

    Glasscard (upgraded cards)

---
+++ GlassCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balatro1
+{
+    public class GlassCard : Card
+    { //static functie met chatgpt hulp
+        private static readonly Random _random = new();
+
+        public GlassCard(Suit suit, CardValue value) : base(suit, value) { }
+
+        public override int CalculateBonus(IEnumerable<Card> hand) => 0;
+        public override double CalculateMultiplier(IEnumerable<Card> hand) => 2.0;
+
+        public bool ShouldBreak() => _random.Next(1,5) == 1;
+    }
+}
