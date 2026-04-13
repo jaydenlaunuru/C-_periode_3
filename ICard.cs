f0f504dca2fac619c6b0f04c27d2f0a1836d07d5
jaydenlaunuru <9025846@student.zadkine.nl>
2026-04-13 18:13:47 +0200

    Finale

---
+++ ICard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Balatro1
+{
+    public interface ICard
+    {
+        Suit Suit { get; }
+        CardValue Value { get; }
+        bool IsWild { get; }
+        int CalculateBonus(IEnumerable<Card> hand);
+        double CalculateMultiplier(IEnumerable<Card> hand);
+    }
+}
---
+++ ICombination.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Balatro1
+{
+    public interface ICombination
+    {
+        string Name { get; }
+        int BaseChips { get; }
+        double BaseMultiplier { get; }
+        bool IsMatch(IEnumerable<Card> hand);
+    }
+}
