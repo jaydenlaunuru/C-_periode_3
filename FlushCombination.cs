a46acfbd08b149ad3c93ff81501ed053f5866c86
jaydenlaunuru <9025846@student.zadkine.nl>
2026-04-13 18:07:21 +0200

    Wildcard & Flush toegevoegd

---
+++ FlushCombination.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Balatro1
+{
+    public class FlushCombination : ICombination
+    {
+        public string Name => "Flush";
+        public int BaseChips => 35;
+        public double BaseMultiplier => 4.0;
+
+        public bool IsMatch(IEnumerable<Card> hand)
+        {
+            if (hand.Count() < 5) return false;
+            var suitGroups = hand.Where(c => !c.IsWild).GroupBy(c => c.Suit);
+            int wildCount = hand.Count(c => c.IsWild);
+            return suitGroups.Any(g => g.Count() + wildCount >= 5);
+        }
+    }
+}
---
+++ WildCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Balatro1
+{
+    public class WildCard : Card
+    {
+        public WildCard(Suit suit, CardValue value) : base(suit, value) { }
+
+        public override bool IsWild => true;
+        public override int CalculateBonus(IEnumerable<Card> hand) => 0;
+        public override double CalculateMultiplier(IEnumerable<Card> hand) => 1.0;
+    }
+}
