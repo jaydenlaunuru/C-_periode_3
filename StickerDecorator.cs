406e41179180eb232c5f8bc06eb60150f48281b4
jaydenlaunuru <9025846@student.zadkine.nl>
2026-04-13 18:13:34 +0200

    Card decoration

---
+++ StickerDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Balatro1
+{
+    public class StickerDecorator : Card
+    {
+        private Card _card;
+        private int _extraPoints;
+
+        public StickerDecorator(Card card, int extraPoints)
+            : base(card.Suit, card.Value)
+        {
+            _card = card;
+            _extraPoints = extraPoints;
+        }
+
+        public override int CalculateBonus(IEnumerable<Card> hand)
+            => _card.CalculateBonus(hand) + _extraPoints;
+
+        public override double CalculateMultiplier(IEnumerable<Card> hand)
+            => _card.CalculateMultiplier(hand);
+
+    }
+}
