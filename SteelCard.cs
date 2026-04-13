807ff928b843768a9e230f1c84a47d832546a3d5
jaydenlaunuru <9025846@student.zadkine.nl>
2026-04-13 18:08:38 +0200

    Steelcard

---
+++ SteelCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Balatro1
+{
+    public class SteelCard : Card
+    {
+        public SteelCard(Suit suit, CardValue value) : base(suit, value) { }
+
+        public override int CalculateBonus(IEnumerable<Card> hand) => 0;
+        public override double CalculateMultiplier(IEnumerable<Card> hand) => 1.0;
+
+        public double PassiveMultiplier => 1.5;
+    }
+}
