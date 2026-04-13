f6c1a82ff7511b80b7de1f4d650ff82207e19f13
jaydenlaunuru <9025846@student.zadkine.nl>
2026-04-13 18:04:15 +0200

    Bonus card & Extra card

---
+++ BonusCard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Balatro1
+{
+    public class BonusCard : Card
+    {
+        public BonusCard(Suit suit, CardValue value) : base(suit, value) { }
+
+        public override int CalculateBonus(IEnumerable<Card> hand) => 10;
+        public override double CalculateMultiplier(IEnumerable<Card> hand) => 1.0;
+    }
+}
---
+++ ExtraCard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Balatro1
+{
+    public class ExtraCard : Card
+    {
+        public ExtraCard(Suit suit, CardValue value) : base(suit, value) { }
+
+        public override int CalculateBonus(IEnumerable<Card> hand)
+        {
+            int count = hand.Count(c => c.Value == this.Value);
+            return count * 2;
+        }
+
+        public override double CalculateMultiplier(IEnumerable<Card> hand) => 1.0;
+    }
+}
