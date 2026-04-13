7d73fa8c74c3ef212d452be6dd5f942aa2293fdb
jaydenlaunuru <9025846@student.zadkine.nl>
2026-04-13 18:08:27 +0200

    Pair combination toegevoegd

---
+++ PairCombination.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Balatro1
+{
+    public class PairCombination : ICombination
+    {
+        public string Name => "Pair";
+        public int BaseChips => 10;
+        public double BaseMultiplier => 2.0;
+
+        public bool IsMatch(IEnumerable<Card> hand)
+        {
+            int wildCount = hand.Count(c => c.IsWild);
+            var groups = hand.Where(c => !c.IsWild).GroupBy(c => c.Value);
+
+            if (groups.Any(g => g.Count() >= 2)) return true;
+            if (wildCount >= 1 && hand.Count(c => !c.IsWild) >= 1) return true;
+            if (wildCount >= 2) return true;
+
+            return false;
+        }
+    }
+}
